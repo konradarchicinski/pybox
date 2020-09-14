686015670f038e9e66f911aa5036d783e0c25c33
Konrad Archicinski <konrad.archicinski@gmail.com>
2020-09-15 00:37:07 +0200

    Updating information in modules containing Table objects.

--- analyticspy/tools/data.py
+++ analyticspy/tools/data.py
@@ -5,36 +5,34 @@ from sys import getsizeof
 from tabulate import tabulate
 from itertools import zip_longest
 from IPython.core.display import display, HTML
+from typing import List, Union, Any, Iterator, NewType
+
+ArrowTable = NewType("ArrowTable", pa.Table)
 
 
 class Table:
-    """[summary]
+    """Heterogeneous, size-mutable data structure."""
 
-    Returns:
-        [type]: [description]
-    """
     __slots__ = ["data"]
 
-    def __init__(self, data=None):
-        """[summary]
-
-        Args:
-            data ([type], optional): [description]. Defaults to None.
-        """
+    def __init__(self, data: dict = None):
+        # TODO add table creation also from different types than dict.
         if data:
-            types = [(name, "O") for name in data.keys()]
-            self.data = np.array(
-                list(zip_longest(*data.values())),
-                dtype=types)
+            if isinstance(data, dict):
+                types = [(name, "O") for name in data.keys()]
+                self.data = np.array(
+                    list(zip_longest(*data.values())),
+                    dtype=types)
+            else:
+                raise TypeError(
+                    f"{type(data)}"
+                    "is an unsupported type for creating a Table object."
+                )
         else:
             self.data = np.array([])
 
-    def __str__(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
+    def __str__(self) -> str:
+        """Display a string representation for a particular Table."""
         print(self.info, "\n")
         return tabulate(
             self.data,
@@ -44,12 +42,8 @@ class Table:
             disable_numparse=True
         )
 
-    def __repr__(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
+    def __repr__(self) -> str:
+        """Display a html string representation for a particular Table."""
         html = "<table>\n<tr>\n<th></th>\n"
 
         for column, type_value in self.data.dtype.fields.items():
@@ -68,15 +62,7 @@ class Table:
         display(HTML(html))
         return self.info
 
-    def __getitem__(self, selector):
-        """[summary]
-
-        Args:
-            selector ([type]): [description]
-
-        Returns:
-            [type]: [description]
-        """
+    def __getitem__(self, selector: Union[tuple, str]):
         if isinstance(selector, tuple):
             # TODO write code for handling slices, itnegers strings and more.
             name, index = selector
@@ -84,13 +70,7 @@ class Table:
         elif isinstance(selector, str):
             return self.data[selector]
 
-    def __setitem__(self, selector, value):
-        """[summary]
-
-        Args:
-            selector ([type]): [description]
-            value ([type]): [description]
-        """
+    def __setitem__(self, selector: Union[tuple, str], value: Any):
         if isinstance(selector, tuple):
             # TODO write code for handling slices, itnegers strings and more.
             name, index = selector
@@ -100,59 +80,61 @@ class Table:
         elif isinstance(selector, str):
             if selector not in self.names:
                 self.new_column(selector)
-            value = self.value_to_list(value)
-            self.align_length(value)
+            value = self._value_to_list(value)
+            self._align_length(value)
             self.data[selector] = value
 
-    @property
-    def data_types(self):
-        """[summary]
+    def _value_to_list(self, value: list) -> list:
+        """Return a list values of supplied column in a list type.
 
-        Returns:
-            [type]: [description]
+        Args:
+            value (list): list containg values of newly added column.
         """
+        if not isinstance(value, list):
+            value = [value]
+        return value
+
+    def _align_length(self, value: list):
+        """Align lenght of columns in entire table.
+
+        Args:
+            value (list): list containg values of newly added column.
+        """
+        length_difference = len(value) - self.length
+        if length_difference > 0:
+            for _ in range(1, length_difference + 1):
+                self.data.resize(self.length + 1)
+                self.data[-1] = tuple([None] * self.width)
+
+    @property
+    def data_types(self) -> List[tuple]:
+        """Return list of column names wiht their data types."""
         if self.data.size == 0:
             return list()
         else:
             return self.data.dtype.descr
 
     @property
-    def length(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
+    def length(self) -> int:
+        """Return length of a Table, in other words number of rows in it."""
         return len(self.data)
 
     @property
-    def width(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
+    def width(self) -> int:
+        """Return width of a Table, in other words number of columns in it."""
         return len(self.data_types)
 
     @property
-    def names(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
+    def names(self) -> List[str]:
+        """Return list of column names."""
         if self.data.size == 0:
             return list()
         else:
             return list(self.data.dtype.names)
 
     @property
-    def to_arrow(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
+    def to_arrow(self) -> ArrowTable:
+        """Return arrow table object from Table."""
         arrow_table = pa.Table.from_arrays(
             [pa.array(self.data[name]) for name in self.names],
             names=self.names
@@ -160,12 +142,8 @@ class Table:
         return arrow_table
 
     @property
-    def info(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
+    def info(self) -> str:
+        """Return information about shape and bytesize of the Table"""
         info = (
             "Table("
             f"shape=({self.width}x{self.length}),"
@@ -174,15 +152,14 @@ class Table:
         )
         return info
 
-    def rows(self, start=None, stop=None):
-        """[summary]
+    def rows(self, start: int = None, stop: int = None) -> Iterator[tuple]:
+        """Return an iterator containing the range of Table rows in tuples.
 
         Args:
-            start ([type], optional): [description]. Defaults to None.
-            stop ([type], optional): [description]. Defaults to None.
-
-        Returns:
-            [type]: [description]
+            start (int, optional): first row of the range, included in the
+                iteration. Defaults to None.
+            stop (int, optional): last row of the range, excluded from  the
+                iteration. Defaults to None.
         """
         if not start:
             start = 0
@@ -190,16 +167,16 @@ class Table:
             stop = self.length
         return (row for row in self.data[start:stop])
 
-    def indices(self, start=None, stop=None, step=None):
-        """[summary]
+    def indices(self, start: int = None, stop: int = None,
+                step: int = None) -> Iterator[int]:
+        """Return an iterator containing the range of Table indices.
 
         Args:
-            start ([type], optional): [description]. Defaults to None.
-            stop ([type], optional): [description]. Defaults to None.
-            step ([type], optional): [description]. Defaults to None.
-
-        Returns:
-            [type]: [description]
+            start (int, optional): start of the range, included in the
+                iteration. Defaults to None.
+            stop (int, optional): end of the range, excluded from the
+                iteration. Defaults to None.
+            step (int, optional): steps between range elements. Defaults to None.
         """
         if not start:
             start = 0
@@ -209,36 +186,11 @@ class Table:
             step = 1
         return range(start, stop, step)
 
-    def value_to_list(self, value):
-        """[summary]
+    def new_column(self, name: str):
+        """Create a new table column.
 
         Args:
-            value ([type]): [description]
-
-        Returns:
-            [type]: [description]
-        """
-        if not isinstance(value, list):
-            value = [value]
-        return value
-
-    def align_length(self, value):
-        """[summary]
-
-        Args:
-            value ([type]): [description]
-        """
-        length_difference = len(value) - self.length
-        if length_difference > 0:
-            for _ in range(1, length_difference + 1):
-                self.data.resize(self.length + 1)
-                self.data[-1] = tuple([None] * self.width)
-
-    def new_column(self, name):
-        """[summary]
-
-        Args:
-            name ([type]): [description]
+            name (str): name of the newly created table column.
         """
         arrays_list = [list(self.data[name]) for name in self.names]
         arrays_list.append([None] * self.length)
--- analyticspy/tools/data_typed.py
+++ analyticspy/tools/data_typed.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
 import numpy as np
+from typing import List, Union
 from itertools import zip_longest
 from .data import Table
 
 
 class TypedTable(Table):
-    """[summary]
+    """Homogeneous, size-mutable, typed data structure."""
 
-    Args:
-        Table ([type]): [description]
-    """
     __slots__ = ["data"]
 
-    def __init__(self, data=None, types=None):
-        """[summary]
-
-        Args:
-            data ([type], optional): [description]. Defaults to None.
-        """
+    def __init__(self, data: dict = None, types: List[tuple] = None):
         if data:
             if not types:
                 raise AttributeError("There is no types list supplied.")
-            self.data = np.array(
-                list(zip_longest(*data.values())),
-                dtype=types)
+            if isinstance(data, dict):
+                self.data = np.array(
+                    list(zip_longest(*data.values())),
+                    dtype=types)
+            else:
+                raise TypeError(
+                    f"{type(data)}"
+                    "is an unsupported type for creating a Table object."
+                )
         else:
             self.data = np.array([])
 
-    def new_column(self, name, data_type):
-        """[summary]
+    def new_column(self, name: str, data_type: Union[type, str]):
+        """Create a new table column of supplied data type.
 
         Args:
-            name ([type]): [description]
+            name (str): name of the newly created table column.
+            data_type (type, str): type of the newly created table column.
         """
         arrays_list = [list(self.data[name]) for name in self.names]
         arrays_list.append([None] * self.length)
