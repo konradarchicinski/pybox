6243b992c39a66c6ab1981c9e593c02f0777a29a
Konrad Archicinski <konrad.archicinski@gmail.com>
2020-10-12 00:37:49 +0200

    Adding an initial version of the `NewsReader` class with its implementation for` Reuters` articles. Selenium Python bindings were used in the class.

--- pybox/tools/scraper/news_reader.py
+++ pybox/tools/scraper/news_reader.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python
-import os
-import importlib
-import datetime
-from abc import ABC, abstractmethod
+from pybox.GLOBALS import APP_PATH
+from pybox.tools.data.data_table import DataTable
 
-import pybox.tools.data.data_table as btddt
+import os
+from datetime import datetime
+from selenium import webdriver
+from importlib import import_module
+from abc import ABC, abstractproperty
 
 
 class NewsReader(ABC):
+    """[summary]"""
 
     def __init__(self):
-        self.news_data = btddt.DataTable(names=["Date", "Header", "Body"],
-                                         dtypes=[datetime.date, str, str])
+        self.news_data = DataTable(
+            names=["Date", "Label", "Headline", "StoryAddress", "Body"],
+            dtypes=[datetime, str, str, str, str])
 
-    def save_news(self):
-        # TODO
-
-    def store_page_source(self, page):
-        # TODO
-
-    @abstractmethod
-    def separate_news_section(self):
-        pass
-
-    @abstractmethod
-    def read_news(self):
-        pass
-
-    @ staticmethod
+    @staticmethod
     def initiate(source, reader_settings=None):
+        """[summary]
+
+        Args:
+            source ([type]): [description]
+            reader_settings ([type], optional): [description]. Defaults to None.
+        """
         if not reader_settings:
             reader_settings = dict()
         for module in os.listdir(os.path.dirname(__file__)):
             if module == f"news_reader_{source.lower()}.py":
-                importlib.import_module(f".{module[:-3]}", __package__)
+                import_module(f".{module[:-3]}", __package__)
                 child_classes = NewsReader.__subclasses__()
+                # TODO rather check name standard = `NewsReader{Source}`.
                 if len(child_classes) > 1:
                     raise ValueError(
                         f"Module `{module}` has been inspected",
@@ -42,3 +39,22 @@ class NewsReader(ABC):
                         "therefore, it is wrongly constructed."
                     )
                 return child_classes[0](**reader_settings)
+
+    def setup_driver(self, page_address):
+        """[summary]
+
+        Args:
+            page_address (str): [description]
+        """
+        self.driver = webdriver.Chrome(APP_PATH + "/chromedriver.exe")
+        self.driver.get(page_address)
+
+    @abstractproperty
+    def read_news_headlines(self):
+        """[summary]"""
+        pass
+
+    @abstractproperty
+    def retrive_headline_content(self):
+        """[summary]"""
+        pass
--- pybox/tools/scraper/news_reader_reuters.py
+++ pybox/tools/scraper/news_reader_reuters.py
@@ -1,22 +1,76 @@
 #!/usr/bin/env python
 from pybox.tools.scraper.news_reader import NewsReader
+from pybox.tools.data.data_helpers import to_date
 
-import datetime
+from time import sleep
+from selenium.webdriver.common.keys import Keys
 
 
-class ReutersNewsReader(NewsReader):
+class NewsReaderReuters(NewsReader):
+    """[summary]"""
 
-    def __init__(self, start_date):
+    def __init__(self, start_date, reuters_page=None):
         super().__init__()
+        self.start_date = to_date(start_date, "datetime")
+        if not reuters_page:
+            self.reuters_page = "https://www.reuters.com/theWire"
+
+    @property
+    def read_news_headlines(self):
+        """[summary]"""
+        self.setup_driver(self.reuters_page)
+        self.accept_cookies
+
+        main_window = self.driver.current_window_handle
+
+        stories = self.driver.find_elements_by_tag_name("article")
+        for story in stories:
+
+            story.find_element_by_tag_name("a").send_keys(
+                Keys.CONTROL + Keys.RETURN)
+
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            self.retrive_headline_content
+
+            self.driver.close()
+            self.driver.switch_to.window(main_window)
+
+        self.driver.close()
+        # TODO finish collecting articles:
+        #   - add switching to the next (archive) pages of headlines
+        #     until the scraper reaches the start date.
+
+    @property
+    def retrive_headline_content(self):
+        """[summary]"""
+        info_bar = self.driver.find_element_by_css_selector(
+            "div[class^='ArticleHeader']")
+
+        time_tags = info_bar.find_elements_by_tag_name('time')[:2]
+        story_date = to_date(
+            " ".join([time.text for time in time_tags]), "datetime")
+        # TODO validate the date transformation.
+        if story_date > self.start_date:
+            label = info_bar.find_element_by_tag_name('a').text
+
+            headline = self.driver.find_element_by_css_selector(
+                "h1[class^='Headline']").text
+
+            story_address = self.driver.current_url
+
+            paragraphs = self.driver.find_elements_by_css_selector(
+                "p[class^='Paragraph']")
+            body = "\n".join([p.text for p in paragraphs])
+
+            self.news_data.insert_row(
+                [story_date, label, headline, story_address, body])
+
+    @property
+    def accept_cookies(self):
+        """Accepts page cookies usage."""
         try:
-            self.start_date = datetime.datetime.strptime(
-                start_date, '%Y-%m-%d %H:%M')
-        except ValueError:
-            self.start_date = datetime.datetime.strptime(
-                start_date, '%Y-%m-%d')
-
-    def separate_news_section(self):
-        pass
-
-    def read_news(self):
-        pass
+            sleep(3)
+            self.driver.find_element_by_id(
+                "_evidon-banner-acceptbutton").click()
+        except Exception:
+            pass
