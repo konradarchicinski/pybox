21d6ae0533d40c28fe50196561ffa422ed6422b2
Konrad Archicinski <konrad.archicinski@gmail.com>
2020-11-28 20:49:01 +0100

    Adding possibility to run additional scrap requests at once, new function calculates provided dates interval and orders the execution of subsequent ones of the same length.

--- pybox/tasks/scrapers/scrap_news.py
+++ pybox/tasks/scrapers/scrap_news.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python
 from pybox.GLOBALS import DATA_PATH
-from pybox.tools.task import TaskInit
+from pybox.tools.task import Task
 from pybox.tools.scraper.news_reader import NewsReader
 
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+
+
+def update_reader_settings(iteration, reader_settings):
+    """Auxiliar function, returns the settings passed to the reader
+    with updated dates. Updated dates are shifted always by the same
+    interval defined by the difference between NewestNewsDate and
+    the OldestNewsDate.
+
+    Args:
+
+        iteration (int): number of current iteration.
+        reader_settings (dict): contains reader settings to be updated.
+    """
+    if iteration > 0:
+        newest_news_date = datetime.strptime(
+            reader_settings["NewestNewsDate"], "%Y-%m-%d")
+        oldest_news_date = datetime.strptime(
+            reader_settings["OldestNewsDate"], "%Y-%m-%d")
+        delta = newest_news_date - oldest_news_date
+
+        reader_settings["OldestNewsDate"] = str(
+            (oldest_news_date - delta).date())
+        reader_settings["NewestNewsDate"] = str(
+            (newest_news_date - delta).date())
+
+    return reader_settings
 
 
 def scrap_news(settings):
@@ -16,35 +42,48 @@ def scrap_news(settings):
         settings (dict): contains settings of this task.
     """
     source = settings["Source"]
+    additional_interval = settings["AdditionalInterval"]
     reader_settings = {key: settings[key]
-                       for key in settings if key != "Source"}
+                       for key in settings
+                       if key not in ["Source", "AdditionalInterval"]}
 
-    reader = NewsReader.initiate(source, reader_settings)
-    reader.read_archival_news
-    reader.news_to_parquet(data_directory=DATA_PATH)
+    for iteration in range(1 + additional_interval):
+        reader_settings = update_reader_settings(iteration, reader_settings)
+
+        reader = NewsReader.initiate(source, reader_settings)
+        reader.read_archival_news
+        reader.news_to_parquet(data_directory=DATA_PATH)
 
 
-task = TaskInit(
+task = Task(
     task_name="NewsScrap",
     task_info="""
     Task scraps and stores news/stories from supplied webpage.
     """)
 task.add_setting(
     name="Source",
-    value="Reuters",
+    default_value="Reuters",
     info="""
     The name of the source webpage such as `Reuters` or `Bloomberg`.
     """)
 task.add_setting(
     name="NewestNewsDate",
-    value=str(date.today()),
+    default_value=str(date.today()),
     info="""
     Date representing newest news that can be stored.
     """)
 task.add_setting(
     name="OldestNewsDate",
-    value=str(date.today() - timedelta(days=1)),
+    default_value=str(date.today() - timedelta(days=1)),
     info="""
     Date representing oldest news that can be stored.
+    """)
+task.add_setting(
+    name="AdditionalInterval",
+    default_value=0,
+    info="""
+    Number of additional intervals. It provides functionality
+    to scrap data from older dates (shifted with the same interval
+    as one between dates provided in initial settings).
     """)
 task.run(main_function=scrap_news)
