e26c198a1ff071092e89e1f676a281509d54bd57
Konrad Archicinski <konrad.archicinski@gmail.com>
2021-04-06 19:29:43 +0200

    Removing irrelevant math code replaced by maths.

--- pybox/math/numbers_generator.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef NUMBERS_GENERATOR_HPP
-#define NUMBERS_GENERATOR_HPP
-
-std::vector<float> from_normal_distribution(float mean, float std, int data_lenght);
-std::vector<float> from_student_t_distribution(float nu, int data_lenght);
-
-#endif
