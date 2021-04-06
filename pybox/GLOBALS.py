78c55d571d0d3051a66a7e46cac7fee3c7d0b311
Konrad Archicinski <konrad.archicinski@gmail.com>
2021-04-06 19:16:50 +0200

    Adding new global variables.

--- pybox/GLOBALS.py
+++ pybox/GLOBALS.py
@@ -8,11 +8,12 @@ from pathlib import Path
 
 APP_PATH = str(Path(os.path.realpath(__file__)).parents[1])
 
-GLOBAL_DATA_PATH = "\\".join([APP_PATH, "data"])
-EXTERNALS_PATH = "\\".join([APP_PATH, "externals"])
-BOXES_PATH = "\\".join([APP_PATH, "boxes"])
+GLOBAL_DATA_PATH = "/".join([APP_PATH, "data"])
+EXTERNALS_PATH = "/".join([APP_PATH, "externals"])
+BOXES_PATH = "/".join([APP_PATH, "boxes"])
+TESTS_PATH = "/".join([APP_PATH, "tests"])
 
-LOGGING_CONFIG = "\\".join([APP_PATH, "config.yml"])
+LOGGING_CONFIG = "/".join([APP_PATH, "config.yml"])
 
 
 with open(LOGGING_CONFIG, 'r') as stream:
